c6ab07bc6a454bd1b952c526e8051d1eef28bf5a
groszek20 <pea@op.pl>
2017-12-04 23:37:21 +0100

    nieudane podejście do JS v.01

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
 <html>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
 </html>
 <?php
-
 echo '
+<script type="text/javascript">
+function clean(e){        
+var textField = document.getElementById(e);
+var regex = /[^a-z 0-9?!.,]/gi;
+if (textField.value.search(regex) > - 1){
+document.getElementById('status').innerHTML = "nieprawidłowy login";
+}else {
+document.getElementById('status').innerHTML = "ok";
+}
+textField.value = textField.value.replace(regex,"");
+}
+</script>
 
 <head>
 <link rel="stylesheet" href="css/screen.css" media="screen" type="text/css"  />
@@ -13,7 +24,8 @@ echo '
 <body>
 <div class="login_form">
 <form method="POST" action="rejestracja.php">
-<span>Login:</span> <input type="text" name="login"><br><br>
+<span>Login:</span> <input type="text" id="ta" onekeyup="clean('ta')" onekeydown="clean('ta')" name="login"><br><br>
+<div id="status"></div>
 <span>Hasło:</span> <input type="password" name="haslo"><br><br>
 <span>Email:</span> <input type="text" name="email"><br><br>
 <input class="button" type="submit" value="Utwórz konto" name="rejestruj">
--- rejestracja.php
+++ rejestracja.php
@@ -7,6 +7,6 @@ $login = $_POST['login'];
 $haslo = $_POST['haslo'];
 $email = $_POST['email'];
 
-echo "Twój login: ".$login.". <br />";
-echo "Twóje hasło: ".$haslo.". <br />";
-echo "Twój email: ".$email.". <br />";
+echo "Twój login: ".$login."<br />";
+echo "Twóje hasło: ".$haslo."<br />";
+echo "Twój email: ".$email."<br />";
